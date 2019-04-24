abf8c1fa4ef01e6f43939c3372447645793310b8
Dooko <andrewcreym@gmail.com>
2019-04-24 09:53:55 +0700

    test

---
+++ testci/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int A, B, C;
+
+	cout << "Enter A: ";
+	cin >> A;
+
+	cout << "Enter B: ";
+	cin >> B;
+
+	cout << "AxB= " << A * B << endl << "A+B= " << A + B << endl;
+
+	system("pause");
+	return 0;
+}
