bf84764de529bb7dfd2c7328a85fe21ec2dcbef6
Sergei <49559222+SomeNewAccount@users.noreply.github.com>
2019-04-27 02:40:09 +0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,13 +4,7 @@
 using namespace std;
 
 int main(int argc, char** argv) {
-	int A = stoi(argv[0]), B = stoi(argv[1]);
-
-	cout << "Enter A: ";
-	cin >> A;
-
-	cout << "Enter B: ";
-	cin >> B;
+	int A = atoi(argv[0]), B = atoi(argv[1]);
 
 	cout << "AxB= " << A * B << endl << "A+B= " << A + B << endl;
 
